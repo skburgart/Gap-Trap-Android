1b3b325de2989783c3af431952f74c2122429c3b
Steven Burgart <skburgart@gmail.com>
2014-04-03 21:26:04 -0400

    Replace ref url

--- src/com/skburgart/pothole/net/NetConfig.java
+++ src/com/skburgart/pothole/net/NetConfig.java
@@ -2,7 +2,7 @@ package com.skburgart.pothole.net;
 
 public class NetConfig {
 
-    public static final String BASE_URL = "http://192.252.222.195:8080/pothole";
+    public static final String BASE_URL = "http://skburgart.com/pothole";
 
     public interface Callback {
         public void processReseponse(String response);
