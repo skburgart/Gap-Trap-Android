2a932983d34027309ac9c0d3b151565cfd4d6f38
Steven Burgart <skburgart@gmail.com>
2014-04-04 21:31:10 -0400

    About dialog

--- src/com/skburgart/pothole/MainActivity.java
+++ src/com/skburgart/pothole/MainActivity.java
@@ -1,11 +1,18 @@
 package com.skburgart.pothole;
 
+import com.skburgart.pothole.net.NetConfig;
+
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 
 public class MainActivity extends FragmentActivity {
@@ -30,27 +37,23 @@ public class MainActivity extends FragmentActivity {
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         // Create the adapter that will return fragments
-        mSectionsPagerAdapter = new SectionsPagerAdapter(
-                getSupportFragmentManager(), this);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), this);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         // When swiping between different sections select the corresponding tab
-        mViewPager
-                .setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-                    @Override
-                    public void onPageSelected(int position) {
-                        actionBar.setSelectedNavigationItem(position);
-                    }
-                });
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
 
         // For each of the sections in the app, add a tab to the action bar.
         for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            actionBar.addTab(actionBar.newTab()
-                    .setText(mSectionsPagerAdapter.getPageTitle(i))
-                    .setTabListener(new SimpleTabListener(mViewPager)));
+            actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i)).setTabListener(new SimpleTabListener(mViewPager)));
         }
     }
 
@@ -58,6 +61,36 @@ public class MainActivity extends FragmentActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                showAbout();
+                return true;
+            case R.id.action_settings:
+                Log.i(TAG, "Open Settings");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showAbout() {
+        AlertDialog.Builder about = new AlertDialog.Builder(this);
+        about.setTitle(R.string.about_title);
+        about.setIcon(R.drawable.ic_launcher);
+        about.setMessage(R.string.about_message);
+        about.setPositiveButton(R.string.about_close, null);
+        about.setNegativeButton(R.string.about_website, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(NetConfig.BASE_URL));
+                startActivity(i);
+            }
+        });
+        about.show();
     }
 
     @Override
